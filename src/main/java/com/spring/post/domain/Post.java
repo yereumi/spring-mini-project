109b2ee1fd435b4805c527dca7a1cd076088d889
dudxo <father8475@naver.com>
2025-02-05 20:29:07 +0900

    feat: Post 추가

---
+++ src/main/java/com/spring/post/domain/Post.java
@@ -0,0 +1,37 @@
+package com.spring.post.domain;
+
+import static jakarta.persistence.ConstraintMode.*;
+
+import com.spring.common.domain.TimeBaseEntity;
+import com.spring.user.domain.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Post extends TimeBaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+
+	private String content;
+
+	@ManyToOne
+	@JoinColumn(name = "userId",
+		nullable = false,
+		foreignKey = @ForeignKey(NO_CONSTRAINT))
+	private User user;
+}
