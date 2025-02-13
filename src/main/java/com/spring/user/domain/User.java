bb25d83d1947229c95c6c5ac62ec35660dd67f52
dudxo <father8475@naver.com>
2025-02-13 15:19:19 +0900

    feat: User 컬럼 추가 설정(name, nullable)

--- src/main/java/com/spring/user/domain/User.java
+++ src/main/java/com/spring/user/domain/User.java
@@ -2,23 +2,32 @@ package com.spring.user.domain;
 
 import com.spring.common.domain.TimeBaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity(name = "UserEntity")
+@Getter
 public class User extends TimeBaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
 	private Long id;
+
+	@Column(name = "email", nullable = false)
 	private String email;
 
+	@Column(name = "password", nullable = false)
 	private String password;
 
+	@Column(name = "name", nullable = false)
 	private String name;
 
+	@Column(name = "role", nullable = false)
 	private Role role;
 
 
