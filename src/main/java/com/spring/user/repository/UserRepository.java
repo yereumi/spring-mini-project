f812381ceac39467a83a8d37066d6852aff90e1e
dudxo <father8475@naver.com>
2025-02-13 15:25:17 +0900

    feat: User JPA Repository 추가

---
+++ src/main/java/com/spring/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.spring.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.user.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
