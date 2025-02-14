a9a4fcc4ea150a40e832beede40b47232059a9f5
dudxo <father8475@naver.com>
2025-02-14 09:53:50 +0900

    fix: Entity name 설정 제거로 인한 JPQL 수정

--- src/main/java/com/spring/post/repository/PostRepository.java
+++ src/main/java/com/spring/post/repository/PostRepository.java
@@ -11,6 +11,6 @@ import com.spring.post.domain.Post;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-	@Query("SELECT p FROM PostEntity p JOIN FETCH p.user")
+	@Query("SELECT p FROM Post p JOIN FETCH p.user")
 	List<Post> findAll();
 }
